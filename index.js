54e618c0f549f6732dd26a0632385ded9f45263a
Suryansh Singh Bisen <07suryansh@gmail.com>
2024-07-05 22:16:39 +0530

    added git ignore

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.get('/',(req,res)=>{
+    console.log('hi');
+})
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
